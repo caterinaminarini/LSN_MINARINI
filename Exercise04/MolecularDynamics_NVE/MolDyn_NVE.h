3ab8508f43ddc4abe273769e54c92830e3b4f8b0
caterinaminarini <65969175+caterinaminarini@users.noreply.github.com>
2020-06-23 17:58:44 +0200

    Add files via upload

---
+++ Exercise04/MolecularDynamics_NVE/MolDyn_NVE.h
@@ -0,0 +1,63 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+using namespace std;
+
+
+//parameters, observables
+const int m_props=1000;
+int n_props, iv,ik,it,ie,igofr;
+double walker[m_props];
+double bin_size,nbins;
+
+string k; 
+
+// averages
+double blk_av[m_props],blk_norm;
+double glob_av[m_props],glob_av2[m_props];
+double stima_pot, stima_kin, stima_tot, stima_temp,stima_gdir;
+double err_pot,err_kin, err_tot,err_temp,err_gdir;
+
+
+//configuration
+const int m_part=108;
+double x[m_part],y[m_part],z[m_part],xold[m_part],yold[m_part],zold[m_part];
+double vx[m_part],vy[m_part],vz[m_part];
+
+// thermodynamical state
+int npart;
+double energy,temp,vol,rho,box,rcut;
+
+// simulation
+int nstep, iprint, seed, irestart, fase, nblk;
+double delta;
+
+
+//functions
+void Input(void);
+void Reset(int);
+void Accumulate(void);
+void Averages(int);
+void Move(void);
+void ConfFinal(void);
+void ConfXYZ(int);
+void Measure(void);
+double Force(int, int);
+double Pbc(double);
+double Error(double,double,int);
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
---
+++ Exercise04/MolecularDynamics_NVE/clean.sh
@@ -0,0 +1,7 @@
+rm -rf gas/*epot*
+rm -rf gas/*ekin*
+rm -rf gas/*etot*
+rm -rf gas/*temp*
+rm -rf gas/*gofr*
+rm -rf gas/*gave*
+rm -rf frames/*.xyz
